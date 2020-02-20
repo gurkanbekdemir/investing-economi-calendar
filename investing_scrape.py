257593805812b5010ae5c407fc4d2183b9965737
Gürkan Bekdemir <44949265+gurkanbekdemir@users.noreply.github.com>
2020-02-20 14:23:06 +0300

    Update investing_scrape.py

--- investing_scrape.py
+++ investing_scrape.py
@@ -6,10 +6,10 @@ import time
 
 class Forex():
     def __init__(self,begin,end):
-        source_code =self.driver(begin,end)
+        source_code =self.browser(begin,end)
         self.extract(source_code)
 
-    def driver(self,begin_date,end_date):
+    def browser(self,begin_date,end_date):
         chrome_options = Options()
         user32 = ctypes.windll.user32  # necessary for resolution
         screensize = ("window-size={}x{}").format(user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
